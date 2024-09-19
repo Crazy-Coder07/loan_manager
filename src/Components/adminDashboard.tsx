957e1f55d577f2e6630a08f913eef419a5175282
Aditya Ranjan <108622072+Crazy-Coder07@users.noreply.github.com>
2024-09-19 10:29:35 +0530

    created the backend

---
+++ src/Components/adminDashboard.tsx
@@ -0,0 +1,236 @@
+import React, { useState } from 'react';
+import './adminDashboard.css';
+import downarr from '../images/downarr.png';
+import user from '../images/user.png';
+import msg from '../images/msg.png';
+import noti from '../images/noti.png';
+import sidebarIcon from '../images/sidebar.png';
+import active from "../images/adminimg/active.png";
+import boro from "../images/adminimg/boro.png";
+import cashdis from "../images/adminimg/cashdis.png";
+import cashrece from "../images/adminimg/cashrece.png";
+import loan from "../images/adminimg/loan.png";
+import otherloan from "../images/adminimg/otherloan.png";
+import rapid from "../images/adminimg/rapid.png";
+import saving from "../images/adminimg/saving.png";
+import loansIcon from "../images/adminimg/loan.png";
+import dashboard1 from "../images/adminimg/dashboard1.png";
+import boro1 from "../images/adminimg/boro1.png";
+import loan1 from "../images/adminimg/loan1.png";
+import repay1 from "../images/adminimg/repay1.png";
+import accnt1 from "../images/adminimg/accnt1.png";
+import rep1 from "../images/adminimg/rep1.png";
+import coll1 from "../images/adminimg/coll1.png";
+import accong1 from "../images/adminimg/accong1.png";
+import sav1 from "../images/adminimg/sav1.png";
+import exp1 from "../images/adminimg/exp1.png";
+import sig1 from "../images/adminimg/sig1.png";
+import inv1 from "../images/adminimg/inv1.png";
+import cal1 from "../images/adminimg/cal1.png";
+import sett1 from "../images/adminimg/sett1.png";
+import signout1 from "../images/adminimg/signout1.png";
+import user1 from "../images/adminimg/user1.png";
+import loanpay1 from "../images/adminimg/loanpay1.png";
+
+
+
+const AdminDashboard = () => {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+  const recentLoans = [
+    { user: 'Tom Cruise', details: 'Contact Email not Linked', date: 'June 09, 2021', status: 'PENDING' },
+    { user: 'Matt Damon', details: 'Adding Images to Featured Posts', date: 'June 09, 2021', status: 'PENDING' },
+    { user: 'Robert Downey', details: 'When will I be charged this month?', date: 'June 08, 2021', status: 'PENDING' },
+    { user: 'Christian Bale', details: 'Payment not going through', date: 'June 08, 2021', status: 'PENDING' },
+    { user: 'Henry Cavil', details: 'Unable to add replies', date: 'June 08, 2021', status: 'APPROVED' },
+    { user: 'Chris Evans', details: 'Downtime since last week', date: 'June 08, 2021', status: 'APPROVED' },
+    { user: 'Sam Smith', details: 'Referral Bonus', date: 'June 08, 2021', status: 'PENDING' },
+  ];
+
+  return (
+    <div className={`dashboard-container ${sidebarOpen ? 'dashboard-elements-shifted' : ''}`}>
+      <div className='header'>
+        <div className='headsidbar'>
+          <div className='head1'>CREDIT APP</div>
+          <div style={{ cursor: "pointer" }}><img src={sidebarIcon} alt="" onClick={toggleSidebar} /></div>
+        </div>
+
+        <div className='head3'>
+          <div><img src={noti} alt='not' /></div>
+          <div><img src={msg} alt='not' /></div>
+          <div><img src={user} alt='not' /></div>
+          <div className='payment'>User</div>
+          <div><img src={downarr} alt='not' /></div>
+        </div>
+      </div>
+
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+        <div className="user-section">
+          <img src={user1} alt="User Icon" className="user-icon" />
+          <span className="user-name">John Okoh</span>
+        </div>
+        <ul>
+          <li>
+            <img src={dashboard1} alt="Dashboard" />
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <img src={boro1} alt="Borrowers" />
+            <span>Borrowers</span>
+          </li>
+          <li>
+            <img src={loan1} alt="Loans" />
+            <span>Loans</span>
+          </li>
+          <li>
+            <img src={repay1} alt="Repayments" />
+            <span>Repayments</span>
+          </li>
+          <li>
+            <img src={accnt1} alt="Loan Parameters" />
+            <span>Loan Parameters</span>
+          </li>
+          <li>
+            <img src={rep1} alt="Accounting" />
+            <span>Accounting</span>
+          </li>
+          <li>
+            <img src={coll1} alt="Reports" />
+            <span>Reports</span>
+          </li>
+          <li>
+            <img src={accong1} alt="Collateral" />
+            <span>Collateral</span>
+          </li>
+          <li>
+            <img src={sav1} alt="Access Configuration" />
+            <span>Access Configuration</span>
+          </li>
+          <li>
+            <img src={exp1} alt="Savings" />
+            <span>Savings</span>
+          </li>
+          <li>
+            <img src={sig1} alt="Expenses" />
+            <span>Expenses</span>
+          </li>
+          <li>
+            <img src={inv1} alt="E-signature" />
+            <span>E-signature</span>
+          </li>
+          <li>
+            <img src={cal1} alt="Investor Accounts" />
+            <span>Investor Accounts</span>
+          </li>
+          <li>
+            <img src={sett1} alt="Calendar" />
+            <span>Calendar</span>
+          </li>
+          <li>
+            <img src={signout1} alt="Settings" />
+            <span>Settings</span>
+          </li>
+          <li>
+            <img src={loanpay1} alt="Sign Out" />
+            <span>Sign Out</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="dashboard-elements">
+        <div className='dashboard-card'>
+          <img src={active} alt='Active Users' />
+          <div>
+            <p>200</p>
+            <p>Active Users</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={boro} alt='Borrowers' />
+          <div>
+            <p>100</p>
+            <p>Borrowers</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={cashdis} alt='Cash Disbursed' />
+          <div>
+            <p>550,000</p>
+            <p>Cash Disbursed</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={cashrece} alt='Cash Received' />
+          <div>
+            <p>1,000,000</p>
+            <p>Cash Received</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={saving} alt='Savings' />
+          <div>
+            <p>450,000</p>
+            <p>Savings</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={rapid} alt='Repaid Loans' />
+          <div>
+            <p>30</p>
+            <p>Repaid Loans</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={otherloan} alt='Other Accounts' />
+          <div>
+            <p>10</p>
+            <p>Other Accounts</p>
+          </div>
+        </div>
+        <div className='dashboard-card'>
+          <img src={loan} alt='Loans' />
+          <div>
+            <p>50</p>
+            <p>Loans</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='recent-loans'>
+        <h3>Recent Loans</h3>
+        <table className='loan-table'>
+          <thead>
+            <tr>
+              <th>User Details</th>
+              <th>Customer Name</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {recentLoans.map((loan, index) => (
+              <tr key={index}>
+                <td>{loan.details}</td>
+                <td>{loan.user}</td>
+                <td>{loan.date}</td>
+                <td><span className={`status ${loan.status.toLowerCase()}`}>{loan.status}</span></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className='pagination'>
+          <span>Rows per page: 7</span>
+          <span>1-7 of 1240</span>
+          <span>&lt; Prev | Next &gt;</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
---
+++ src/Components/userDashboard.tsx
@@ -0,0 +1,266 @@
+import React, { useState } from 'react';
+import './userDashboard.css';
+import axios from 'axios'; // Import Axios
+import downarr from '../images/downarr.png';
+import user from '../images/user.png';
+import msg from '../images/msg.png';
+import noti from '../images/noti.png';
+import card from '../images/card.png';
+import budget from '../images/budget.png';
+import payment from '../images/payment.png';
+import home from '../images/home.png';
+import curr from '../images/curr.png';
+import currsym from '../images/currsym.png';
+import loanicon from '../images/loanicon.png';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const UserDashboard = () => {
+  const [borrowcs, setBorrowcs] = useState(true);
+  const [transactcs, setTransactcs] = useState(false);
+  const [depositecs, setDepositecs] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: '',
+    loanAmount: '',
+    loanTenure: '',
+    employmentStatus: '',
+    reasonForLoan: '',
+    employmentAddress: ''
+  });
+
+  const [loans] = useState([
+    { officer: 'John Okoh', amount: 50000, date: 'June 09, 2021', time: '6:30 PM', status: 'PENDING' },
+    { officer: 'John Okoh', amount: 100000, date: 'June 07, 2021', time: '6:30 PM', status: 'VERIFIED' },
+    { officer: 'John Okoh', amount: 100000, date: 'June 07, 2021', time: '6:30 PM', status: 'REJECTED' },
+    { officer: 'John Okoh', amount: 100000, date: 'May 27, 2021', time: '6:30 PM', status: 'APPROVED' },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const rowsPerPage = 2; 
+
+  const indexOfLastLoan = currentPage * rowsPerPage;
+  const indexOfFirstLoan = indexOfLastLoan - rowsPerPage;
+  const currentLoans = loans.slice(indexOfFirstLoan, indexOfLastLoan);
+
+  const totalPages = Math.ceil(loans.length / rowsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Handle input change
+  const handleInputChange = (e:any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  // Handle form submit
+  const handleSubmit = async (e:any) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:8080/api/v1/create-loan', formData); 
+      console.log(response.data);
+      toast.success("loan applied successfully");
+      setIsDialogOpen(false);
+    } catch (error) {
+      console.error(error);
+      alert('Error submitting loan application');
+    }
+  };
+
+  return (
+    <div className='complete'>
+      {/* Header Content */}
+      <ToastContainer />
+      <div className='header'>
+        <div className='head1'>
+          CREDIT APP
+        </div>
+
+        <div className='head2'>
+          <div>
+            <img src={home} alt='not' />
+            <div className='home'>Home</div>
+          </div>
+          <div>
+            <img src={payment} alt='not' />
+            <div className='payment'>Payments</div>
+          </div>
+          <div>
+            <img src={budget} alt='not' />
+            <div className='payment'>Budgets</div>
+          </div>
+          <div>
+            <img src={card} alt='not' />
+            <div className='payment'>Card</div>
+          </div>
+        </div>
+
+        <div className='head3'>
+          <div><img src={noti} alt='not' /></div>
+          <div><img src={msg} alt='not' /></div>
+          <div><img src={user} alt='not' /></div>
+          <div className='payment'>User</div>
+          <div><img src={downarr} alt='not' /></div>
+        </div>
+      </div>
+
+      {/* Body Content */}
+      <div className='bodytp'>
+        <div className='body1'>
+          <div className='body1left'>
+            <div className='currimg'>
+              <img src={curr} alt='not' />
+            </div>
+            <div className='deficit'>
+              <div>DEFICIT</div>
+              <div className='currsymimg'>
+                <img src={currsym} alt='not' />
+              </div>
+            </div>
+            <div className='amnt'>
+              0.0
+            </div>
+          </div>
+
+          <div className='getloan' onClick={() => setIsDialogOpen(true)}>
+            Get A Loan
+          </div>
+        </div>
+
+        <div className='tabs'>
+          <button className={borrowcs ? 'active-tab' : ''} onClick={() => { setBorrowcs(true); setTransactcs(false); setDepositecs(false); }}>
+            Borrow Cash
+          </button>
+          <button className={transactcs ? 'active-tab' : ''} onClick={() => { setBorrowcs(false); setTransactcs(true); setDepositecs(false); }}>
+            Transact
+          </button>
+          <button className={depositecs ? 'active-tab' : ''} onClick={() => { setBorrowcs(false); setTransactcs(false); setDepositecs(true); }}>
+            Deposit Cash
+          </button>
+        </div>
+
+        <div className="body3">
+          <input
+            type="text"
+            placeholder="Search for loans"
+            className="search-bar"
+          />
+        </div>
+      </div>
+
+      {isDialogOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={() => setIsDialogOpen(false)}>&times;</span>
+            <h2>APPLY FOR A LOAN</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="form-row">
+                <input 
+                  type="text" 
+                  name="fullName"
+                  placeholder="Full name as it appears on bank account" 
+                  value={formData.fullName}
+                  onChange={handleInputChange}
+                  required 
+                />
+                <input 
+                  type="number" 
+                  name="loanAmount"
+                  placeholder="How much do you need?" 
+                  value={formData.loanAmount}
+                  onChange={handleInputChange}
+                  required 
+                />
+              </div>
+              <div className="form-row">
+                <input 
+                  type="number" 
+                  name="loanTenure"
+                  placeholder="Loan tenure (in months)" 
+                  value={formData.loanTenure}
+                  onChange={handleInputChange}
+                  required 
+                />
+                <input 
+                  type="text" 
+                  name="employmentStatus"
+                  placeholder="Employment status" 
+                  value={formData.employmentStatus}
+                  onChange={handleInputChange}
+                  required 
+                />
+              </div>
+              <div className="form-row">
+                <textarea 
+                  name="reasonForLoan"
+                  placeholder="Reason for loan" 
+                  value={formData.reasonForLoan}
+                  onChange={handleInputChange}
+                  required
+                />
+                <input 
+                  type="text" 
+                  name="employmentAddress"
+                  placeholder="Employment address" 
+                  value={formData.employmentAddress}
+                  onChange={handleInputChange}
+                  required 
+                />
+              </div>
+              <div className="form-footer">
+                <input type="checkbox" required />
+                <label>I have read the important information and agree to the terms.</label>
+              </div>
+              <button type="submit" className="submit-btn">Submit</button>
+            </form>
+          </div>
+        </div>
+      )}
+
+      <div className="loan-table-container">
+        <h2>Applied Loans</h2>
+        <div className="loan-table">
+          <div className="loan-table-header">
+            <div>Loan Officer</div>
+            <div>Amount</div>
+            <div>Date Applied</div>
+            <div>Status</div>
+          </div>
+          {currentLoans.map((loan, index) => (
+            <div key={index} className="loan-table-row">
+              <div className="loan-officer">
+                <img src={loanicon || 'not-found.png'} alt="not found" className="loan-officer-img" />
+                <div>{loan.officer}</div>
+              </div>
+              <div>{loan.amount.toLocaleString()}</div>
+              <div>
+                <div>{loan.date}</div>
+                <div>{loan.time}</div>
+              </div>
+              <div>
+                <span className={`status-badge ${loan.status.toLowerCase()}`}>{loan.status}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="pagination-controls">
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>Prev</button>
+          <span>Page {currentPage} of {totalPages}</span>
+          <button onClick={handleNextPage} disabled={currentPage === totalPages}>Next</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDashboard;
